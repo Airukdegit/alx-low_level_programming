8021f879e74e20dfb7bd141e2c8a18d9c665bc78
Airuk <kuriakevin254@gmail.com>
2022-03-16 11:13:46 +0300

    Added task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints out the entire alphabet 10 times to the standard output
+ * retun: 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int  count_to_ten = 0;
+
+	while (count_to_ten < 10)
+	{
+		char alphabet = 'a';
+		while (alphabet <= 'z' )
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+		count_to_ten++;
+	}
+
+}
