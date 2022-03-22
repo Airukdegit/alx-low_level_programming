c0973b336bdfc44d2bfcc6c4277f092598d1e51e
Airuk <Airukdegit@gmail.com>
2022-03-22 23:01:41 +0300

    Betty coding style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * reset_to_98 - takes a pointer to a parameter and updates its value.
- * @n: input integer.
- * Return: 0.
- */
- void reset_to_98(int *n)
+* reset_to_98 - takes a pointer to a parameter and updates its value.
+* @n: input integer.
+* Return: 0.
+*/
+void reset_to_98(int *n)
 {
-    *n=98;
+    *n = 98;
 }
