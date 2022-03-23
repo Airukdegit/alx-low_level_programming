4596d2963e4a2a85bc941ecd976f923ef94405f4
Airuk <kuriakevin254@gmail.com>
2022-03-23 14:43:39 +0300

    BettyKernel style code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- *  * rev_string - reverses a string.
- *   * @s: input string.
- *    * Return: no return.
- *     */
+*rev_string - reverses a string.
+*@s: input string.
+*Return: no return.
+*/
 void rev_string(char *s)
 {
 	int count = 0, i, j;
-	char *str, temp;
 	
+	char *str, temp;
 	while (count >= 0)
 	{
 		if (s[count] == '\0')
 			break;
 		count++;
 	}	
+	
 	str = s;
 
 	for (i = 0; i < (count - 1); i++)
