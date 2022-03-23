dfdfd9b0c81413f08049106bf699d859580d7434
Airuk <Airukdegit@gmail.com>
2022-03-23 07:14:31 +0300

    Added files 3-puts.c and 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - Prints a string then a new line. 
+* @str: arg  *str
+* Return: 0.
+*/
+void _puts(char *str);
+{
+	int i = 0
+
+	while (str[i])
+	{
+		_putchar(str[i];
+		i++;
+	}
+	_putchar('\n')
+	return (count);
+}
+
