54289b219b175e6482fc281f415115d28a51dbaf
Airuk <Airukdegit@gmail.com>
2022-03-29 07:21:39 +0300

    Added file 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - Locates char and returns pointer
+ * @s: string
+ * @c: character
+ * Return: the pointer to the first occurence
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}
+
