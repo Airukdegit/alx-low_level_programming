040998c0ffecd4bdb5d6f404e5af938d81b14f3a
Airuk <Airukdegit@gmail.com>
2022-03-29 06:03:57 +0300

    Added task one two

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - copies mamory area
+ * @dest: destinatio memory area
+ * @src: source memory area.
+ * @n: bytes filled
+ * Return: the pointer
+ */
+char *_memcpy(char *dest, char b, usigned int n)
+{	
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+
+	return (dest);
+}
+
