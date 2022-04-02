b2973a16b04deae5024c361b292e21d9d8b8e52e
Airuk <kuriakevin254@gmail.com>
2022-04-02 05:23:14 +0300

    Betty code dtyle

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,14 +9,14 @@
  */
 int isInt(char *s)
 {
-	int i = 0;
-	
-	for(i = 0; s[i] != '\0'; i++)
-	{
-		if(s[i] < '0' || s[i] > '9')
-			return (0);
-	}
-	return (1);
+int i = 0;
+
+for(i = 0; s[i] != '\0'; i++)
+{
+if(s[i] < '0' || s[i] > '9')
+return (0);
+}
+return (1);
 }
 
 /**
@@ -27,28 +27,28 @@ int isInt(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int i = 0, coinUsed = 0, coin = 0;
-	int coins[] = {25, 10, 5, 2, 1};
+int i = 0, coinUsed = 0, coin = 0;
+int coins[] = {25, 10, 5, 2, 1};
 
-	if(argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	if(isInt(argv[1]))
-	{
-		i = atoi(argv[1]);
-		while(i > 0 && coin <=4)
-		{
-			if(i >= coins[coin])
-			{
-				i -= coins[coin];
-				coinUsed++;
-			}
-			else
-				coin++;
-		}
-	}
-	printf("%i\n", coinUsed);
-	return (0);
+if(argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+if(isInt(argv[1]))
+{
+i = atoi(argv[1]);
+while(i > 0 && coin <=4)
+{
+if(i >= coins[coin])
+{
+i -= coins[coin];
+coinUsed++;
+}
+else
+coin++;
+}
+}
+printf("%i\n", coinUsed);
+return (0);
 }
